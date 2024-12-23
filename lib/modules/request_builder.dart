8b92c164e93b7df0c4eb1ca59c16de00dd69cd20
Ilase <igeodec@gmail.com>
2024-12-23 11:10:36 +0300

    page controller in home page

--- lib/modules/request_builder.dart
+++ lib/modules/request_builder.dart
@@ -1,3 +1,1 @@
-// Future<http.Response> postRequest() async {
-//   return null;
-// }
+
--- lib/pages/mt_headpage.dart
+++ lib/pages/mt_headpage.dart
@@ -14,7 +14,21 @@ class MtHeadpage extends StatefulWidget {
   State<MtHeadpage> createState() => _MtHeadpageState();
 }
 
+final List<Widget> _pages = [
+  Center(
+    child: Text('Page 1'),
+  ),
+  Center(
+    child: Text('Page 2'),
+  ),
+  Center(
+    child: Text('Page 3'),
+  ),
+];
+
 class _MtHeadpageState extends State<MtHeadpage> {
+  int _selectedIndex = 0;
+  PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +49,9 @@ class _MtHeadpageState extends State<MtHeadpage> {
             MtDropmenu(
               title: AppLocalizations.of(context)!.appBarActionsButtonTitle,
             ),
+            const SizedBox(
+              width: 10,
+            ),
             MtDropmenu(
               title: AppLocalizations.of(context)!.appBarInfoButtonTitle,
             ),
@@ -44,13 +61,70 @@ class _MtHeadpageState extends State<MtHeadpage> {
           padding: EdgeInsets.all(8),
           child: null,
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(8.0),
+        body: Padding(
+          padding: const EdgeInsets.all(6.0),
           child: Center(
               child: Row(
             children: [
-              MtNavigationRail(),
-              MtDialogSendScanRequest(),
+              NavigationRail(
+                  groupAlignment: -1,
+                  labelType: NavigationRailLabelType.selected,
+                  onDestinationSelected: (int index) {
+                    setState(() {
+                      _selectedIndex = index;
+                      _pageController.animateToPage(index,
+                          duration: const Duration(milliseconds: 700),
+                          curve: Curves.easeInOut);
+                    });
+                  },
+                  destinations: const <NavigationRailDestination>[
+                    NavigationRailDestination(
+                      icon: Icon(
+                        Icons.home,
+                        size: 45,
+                      ),
+                      label: Text('Главная'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(
+                        Icons.radar,
+                        size: 45,
+                      ),
+                      label: Text('Сканирование'),
+                    ),
+                    NavigationRailDestination(
+                        icon: Icon(
+                          Icons.document_scanner,
+                          size: 45,
+                        ),
+                        label: Text('Отчёты'))
+                  ],
+                  selectedIndex: _selectedIndex),
+              Expanded(
+                  child: PageView(
+                physics: const NeverScrollableScrollPhysics(),
+                allowImplicitScrolling: false,
+                controller: _pageController,
+                scrollDirection: Axis.vertical,
+                children: [
+                  Container(
+                    color: Colors.blue[100],
+                    child: const Center(
+                      child: MtDialogSendScanRequest(),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.green[100],
+                    child: const Center(
+                      child: MtDialogSendScanRequest(),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red[100],
+                    child: const Center(child: Text('Info Page')),
+                  ),
+                ],
+              )),
             ],
           )),
         ));
