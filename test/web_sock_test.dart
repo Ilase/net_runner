a7e11d9d7413b422f8d7d0a71df74b5b7e3612c8
ilase <igeodec@gmail.com>
2025-01-19 21:23:28 +0300

    more trash then i write

--- test/web_sock_test.dart
+++ test/web_sock_test.dart
@@ -16,13 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: BlocProvider(
-        create: (context) => WebSocketBloc(
-            'ws://192.168.20.140:80/wsscan',
-            {
-              "uid" : "1378500800859113",
-              "token":"3045022100f9e2e5e01ac12458f7c1f7753d1584a3527fc1d17df0466baf61e3de4a61a2c5022009bfe43ac628ac4d0ff55c2098dee5332c64dfbf5b90f500665988f46e87abef"
-            }
-        ),
+        create: (context) => WebSocketBloc(),
         child: WebSocketExample(),
       ),
     );
