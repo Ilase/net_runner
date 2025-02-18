1d40c9dccfbbdb2a3ee6c6182ef957c7a4c02514
ilase <igeodec@gmail.com>
2025-02-18 20:13:14 +0300

    in test created some example of new page

--- test/ui_tests/drawer_navigation.dart
+++ test/ui_tests/drawer_navigation.dart
@@ -1,126 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
+  State<HomePage> createState() => _HomePageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-  bool _isDrawerOpen = false;
-
-  static const List<Widget> _pages = <Widget>[
-    Icon(Icons.home, size: 150),
-    Icon(Icons.business, size: 150),
-    Icon(Icons.school, size: 150),
-  ];
-
-  static const List<NavigationRailDestination> _navRailDestinations =
-      <NavigationRailDestination>[
-    NavigationRailDestination(
-      icon: Icon(Icons.home),
-      label: Text('Home'),
-    ),
-    NavigationRailDestination(
-      icon: Icon(Icons.business),
-      label: Text('Business'),
-    ),
-    NavigationRailDestination(
-      icon: Icon(Icons.school),
-      label: Text('School'),
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-      _isDrawerOpen = true;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  bool isDrawerExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('NavigationRail and Drawer Example'),
-      ),
       body: Row(
-        children: <Widget>[
-          NavigationRail(
-            selectedIndex: _selectedIndex,
-            onDestinationSelected: _onItemTapped,
-            destinations: _navRailDestinations,
-          ),
-          const VerticalDivider(thickness: 1, width: 1),
-          Expanded(
-            child: Center(
-              child: _pages[_selectedIndex],
+        children: [
+          AnimatedContainer(
+            width: isDrawerExpanded ? 300 : 100,
+            curve: Curves.easeInOut,
+            duration: Duration(milliseconds: 500),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2),
             ),
-          ),
-          if (_isDrawerOpen)
-            Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  DrawerHeader(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                    ),
-                    child: Text(
-                      'Drawer Header',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      AnimatedCrossFade(
+                        firstChild: Text('Page1'),
+                        secondChild: Icon(Icons.ac_unit),
+                        crossFadeState: isDrawerExpanded
+                            ? CrossFadeState.showFirst
+                            : CrossFadeState.showSecond,
+                        duration: Duration(milliseconds: 500),
                       ),
+                      Text('Page 2'),
+                      Text('Page 3'),
+                      Text('Page 4'),
+                      Text('Page 5'),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isDrawerExpanded = !isDrawerExpanded;
+                      });
+                    },
+                    icon: Icon(
+                      Icons.accessible,
                     ),
                   ),
-                  ListTile(
-                    leading: Icon(Icons.home),
-                    title: Text('Home'),
-                    onTap: () {
-                      setState(() {
-                        _isDrawerOpen = false;
-                      });
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.business),
-                    title: Text('Business'),
-                    onTap: () {
-                      setState(() {
-                        _isDrawerOpen = false;
-                      });
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.school),
-                    title: Text('School'),
-                    onTap: () {
-                      setState(() {
-                        _isDrawerOpen = false;
-                      });
-                    },
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
+          ),
+          Expanded(child: Placeholder())
         ],
       ),
     );
