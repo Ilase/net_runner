12a88d8bcda22db644118180cd326812d8f1b756
GreeChel <maksancel032@gmail.com>
2025-02-20 10:13:14 +0300

    Update scanning view state
    _ScanningPgState:
      - udpate expanded widget box
    
     Changes to be committed:
    	modified:   lib/features/hosts/presentation/hosts_pg.dart
    	modified:   lib/features/scanning/presentation/scanning_pg.dart

--- lib/features/scanning/presentation/scanning_pg.dart
+++ lib/features/scanning/presentation/scanning_pg.dart
@@ -12,47 +12,193 @@ class ScanningPg extends StatefulWidget {
 }
 
 class _ScanningPgState extends State<ScanningPg> {
+  Map<String, dynamic>? _selectedItem;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        decoration: const BoxDecoration(),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                offset: Offset(3, 3),
+                blurRadius: 10,
+                color: Colors.grey,
+              ),
+            ]),
         child: Column(
           children: [
-            SizedBox(
-              height: 55,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    children: [
-                      const Text(
-                        'Сканирования*',
-                        //style: AppTheme.lightTheme.textTheme.titleMedium
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pushNamed('/create-scan');
-                          },
-                          child: Text('New scan*'))
-                    ],
+                  IconButton(
+                    onPressed: () {
+                      /// Поиск
+                    },
+                    icon: Icon(Icons.refresh),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(labelText: 'Поиск'),
+                    ),
                   ),
                   IconButton(
                     onPressed: () {},
-                    icon: const Icon(Icons.refresh),
-                  )
-                  // IconButton(
-                  //   onPressed: () => context.read<PostRequestBloc>().add(
-                  //       PostRequestGetEvent(
-                  //           uri: "http://192.168.20.140:3001/api/v1/nmap")),
-                  //   icon: const Icon(Icons.refresh),
-                  // ),
+                    icon: Icon(Icons.search),
+                  ),
                 ],
               ),
             ),
+
+            /// Основное тело
+            Expanded(
+              child: Row(
+                children: [
+                  /// Левая панель
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16.0,
+                        left: 16.0,
+                        right: 16.0,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(15)),
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(3, 3),
+                              blurRadius: 10,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(15),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(3, 3),
+                                blurRadius: 10,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Text('Сканирования*'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  /// Правая панель
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16.0,
+                        left: 16.0,
+                        right: 16.0,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(15)),
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(3, 3),
+                              blurRadius: 10,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(15),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(3, 3),
+                                blurRadius: 10,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Text('Сканирования*'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // SizedBox(
+            //   height: 55,
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Row(
+            //         children: [
+            //           const Text(
+            //             'Сканирования*',
+            //             //style: AppTheme.lightTheme.textTheme.titleMedium
+            //           ),
+            //           const SizedBox(
+            //             width: 5,
+            //           ),
+            //           ElevatedButton(
+            //               onPressed: () {
+            //                 Navigator.of(context).pushNamed('/create-scan');
+            //               },
+            //               child: Text('New scan*'))
+            //         ],
+            //       ),
+            //       IconButton(
+            //         onPressed: () {},
+            //         icon: const Icon(Icons.refresh),
+            //       ),
+            //       Expanded(
+            //         child: Row(
+            //           children: [
+            //             Expanded(
+            //               child: Padding(
+            //                 padding: const EdgeInsets.only(
+            //                   top: 16.0,
+            //                   left: 16.0,
+            //                   right: 16.0,
+            //                 ),
+            //               ),
+            //             )
+            //           ],
+            //         ),
+            //       )
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
